9c9abe57dbbb62b1e58f0bb392ac41fbcbff6324
ffacon <francois.facon@gmail.com>
2016-08-10 16:37:59 +0200

    The (*) prefix to ngFor indicates that the <li> element and its children constitute a master template.

--- app/app.component.ts
+++ app/app.component.ts
@@ -22,19 +22,14 @@ const HEROES: Hero[] = [
     selector: 'my-app',
     template:`
   <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name">
-  </div>
+  <li *ngFor="let hero of heroes">
+  <span class="badge">{{hero.id}}</span> {{hero.name}}
+  </li>
   `
 })
 
 export class AppComponent { 
     title = 'Tour of Heroes';
-    hero : Hero = { 
-        id: 1, 
-        name: 'windstorm' 
-    };
+    public heroes = HEROES;
+
 }
