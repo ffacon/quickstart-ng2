84f5f78666fe295b75a80711ed2838b505d56b7f
ffacon <francois.facon@gmail.com>
2016-08-16 11:05:12 +0200

    extract hero class to ease the reuse

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,7 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { Hero } from './hero';
+
 
 @Component({
   selector: 'my-hero-detail',
 })
 export class HeroDetailComponent {
+    hero: Hero;
+
 }
