c404c14d7dfc2e65a2e7e02b9c65e224d91742af
ffacon <francois.facon@gmail.com>
2016-08-21 14:51:46 +0200

    Delete HEROES and impl onInit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,6 @@
-import { Component } from '@angular/core';
-import { Hero } from './hero';
+import { Component, OnInit } from '@angular/core';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { Hero } from './hero';
 
 @Component({
     selector: 'my-app',
@@ -79,10 +67,12 @@ const HEROES: Hero[] = [
 
 })
 
-export class AppComponent { 
+export class AppComponent implements OnInit {
     title = 'Tour of Heroes';
-    public heroes = HEROES;
+    public heroes = Hero[];
     selectedHero: Hero;
+    
+    ngOnInit() { }
     
     onSelect(hero: Hero) { this.selectedHero = hero; }
 
