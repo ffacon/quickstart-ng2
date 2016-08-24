08c1ef75f149feede13f40bc92c07fe174d5ca5f
ffacon <francois.facon@gmail.com>
2016-08-24 16:20:18 +0200

    Extract and use heroes.component

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
   declarations: [ AppComponent,
-                  HeroDetailComponent],
+                  HeroDetailComponent,
+                  HeroesComponent],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
