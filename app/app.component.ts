04e7b86b958433af923fad4dacb5ef444adbb05c
ffacon <francois.facon@gmail.com>
2016-08-28 16:45:33 +0200

    add router-outlet

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,7 +9,8 @@ import { HeroesComponent } from './heroes.component';
     providers: [HeroService],
     template:`
     <h1>{{title}}</h1>
-    <my-heroes>My Heroes</my-heroes>`,
+    <a routerLink="/heroes">Heroes</a>
+    <router-outlet></router-outlet>`,
  
 })
 
