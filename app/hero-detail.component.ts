b027034910da6ed7b5c80044ec46241b97611a82
ffacon <francois.facon@gmail.com>
2016-09-01 15:50:52 +0200

    get selected hero form ActivatedRoute params instead of @input

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
 
 
@@ -12,10 +15,26 @@ import { Hero } from './hero';
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
         </div>
+        <button (click)="goBack()">Back</button>
     </div>`,
 })
-export class HeroDetailComponent {
-    @Input()
+export class HeroDetailComponent implements OnInit{
     hero: Hero;
+  
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute) { }
 
+    ngOnInit() {
+    this.route.params.forEach((params: Params) => {
+      let id = +params['id'];
+      this.heroService.getHero(id)
+        .then(hero => this.hero = hero);
+        });
+    }
+    
+    goBack() { 
+        window.history.back(); 
+    }
+  
 }
